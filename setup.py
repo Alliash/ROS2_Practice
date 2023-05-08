745c1623bd1c48333dbf380e50229150a2b70253
Alliash <111270327+Alliash@users.noreply.github.com>
2023-05-08 13:22:36 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+entry_points={
+    'console_scripts': [
+    	'helloworld_subscriber = topic_helloworld.helloworld_subscriber:main',
+    	'helloworld_publisher = topic_helloworld.helloworld_publisher:main'
+    ],
+},
